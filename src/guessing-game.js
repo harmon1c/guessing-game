bbe9d6b02ea022ef05042a9c71c9dad94cea88e1
harmon1c <80057115+harmon1c@users.noreply.github.com>
2023-10-23 13:31:50 +0300

    feat: add task solution 2023-10-23 13:30:56

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    //set defaults
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.solution = 0;
+    }
+    //range
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+    //mean value
     guess() {
-
+      this.solution = Math.ceil((this.min + this.max) / 2);
+      return this.solution;
     }
-
+    //if guess value is too high -> lower range to upper mean
     lower() {
-
-    }
-
+      this.max = this.solution;
+    }w
+    //if guess value is too low -> lower range to lower mean
     greater() {
-
+      this.min = this.solution;
     }
 }
 
